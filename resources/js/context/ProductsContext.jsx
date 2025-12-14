a5a7660a437aaa9d90896b617c9b148c6a0e79d5
deto <jorgerubilarssj@gmail.com>
2025-12-13 21:53:36 -0300

    changed ProductsContext.jsx

--- resources/js/context/ProductsContext.jsx
+++ resources/js/context/ProductsContext.jsx
@@ -3,32 +3,44 @@ import { createContext, useContext, useEffect, useState } from "react";
 import Config from "../Config";
 
 // Es para crear el contexto (es como una caja donde se guardan datos para no pasar lso props)
-const ProductsContext = createContext();
+const ProductsContext = createContext(null);
 
 // Es para envolver la app y permite que todos los componentes dentro accedan a los datos
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const loadAllProducts = async () => {
+  const loadData = async () => {
+    setLoading(true);
+    setError(null);
+
     try {
-      const { data } = await Config.getAllProducts(); 
-      setProducts(data ?? []);
+      const [productsRes, categoriesRes] = await Promise.all([
+        Config.getAllProducts(),
+        Config.getAllCategories(),
+      ]);
+
+      setProducts(productsRes?.data ?? []);
+      setCategories(categoriesRes?.data ?? []);
     } catch (err) {
-      console.error("Error cargando productos:", err);
+      console.error("Error cargando datos:", err);
       setProducts([]);
+      setCategories([]);
+      setError("Error al cargar productos o categorías");
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    loadAllProducts();
+    loadData();
   }, []);
 
   return (
     // al colocar los products y el loading en el value hace que cualquier componente pueda leerlos usando el useProducts que se define abajo
-    <ProductsContext.Provider value={{ products, loading }}>
+    <ProductsContext.Provider value={{ products, categories, loading, error}}>
       {children}
     </ProductsContext.Provider>
   );
