0910097068894aedbfcbbaa1e8d58abb718cd3fd
deto <jorgerubilarssj@gmail.com>
2025-12-13 21:52:05 -0300

    changed SectionHeader.jsx

--- resources/js/components/SectionHeader.jsx
+++ resources/js/components/SectionHeader.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { useProducts } from "../context/ProductsContext";
+import LoadingBar from "./LoadingBar";
 
-const SectionHeader = ({title, description}) => {
+const SectionHeader = ({ id }) => {
+  const { categories, loading } = useProducts();
+
+  if (loading) return <LoadingBar />;
+
+  const category = categories.find(c => c.id === id);
+
+  if (!category) {
+    return <h2>Categoría no encontrada</h2>;
+  }
+
   return (
     <div>
-      <h1>{title}</h1>
-      <h3>{description}</h3>
+      <h1>{category.name}</h1>
+      <h3>{category.description}</h3>
     </div>
-  )
-}
+  );
+};
 
 export default SectionHeader
