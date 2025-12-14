d341fe0e23e4b6bf9e2d0245e42ed77fa8400019
deto <jorgerubilarssj@gmail.com>
2025-12-13 21:51:02 -0300

    added /categories Route in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 
 // Public
 
 Route::get('/products', [ProductController::class,'allProducts']);
+Route::get('/categories', [CategoryController::class,'allCategories']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
