7cd7c5fcabf15c4c67d1010857cda09c6f5cdfc0
detossj <jorgerubilarssj@gmail.com>
2025-12-16 11:49:16 -0300

    added /promotions Route

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PromotionController;
 
 
 // Public
 
 Route::get('/products', [ProductController::class,'allProducts']);
 Route::get('/categories', [CategoryController::class,'allCategories']);
+Route::get('/promotions', [PromotionController::class,'allPromotions']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
