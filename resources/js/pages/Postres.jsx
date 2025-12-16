862fb5359eadbadaa27ca63a62031c7ba457ba11
detossj <jorgerubilarssj@gmail.com>
2025-12-16 11:10:22 -0300

    changed import of ItemsList.jsx to ProductsList.jsx

--- resources/js/pages/Postres.jsx
+++ resources/js/pages/Postres.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ItemsList from '../components/ItemsList'
+import ProductsList from '../components/ProductsList'
 import SectionHeader from '../components/SectionHeader'
 
 const Postres = () => {
   return (
     <>
       <SectionHeader id={5} />
-      <ItemsList id={5} />
+      <ProductsList id={5} />
     </>
   )
 }
