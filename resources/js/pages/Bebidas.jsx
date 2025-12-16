cdefe8e46d9c0a34053bc083299fe3430061ffd7
detossj <jorgerubilarssj@gmail.com>
2025-12-16 11:10:05 -0300

    changed import of ItemsList.jsx to ProductsList.jsx

--- resources/js/pages/Bebidas.jsx
+++ resources/js/pages/Bebidas.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ItemsList from '../components/ItemsList'
+import ProductsList from '../components/ProductsList'
 import SectionHeader from '../components/SectionHeader'
 
 const Bebidas = () => {
   return (
     <>
       <SectionHeader id={4} />
-      <ItemsList id={4} />
+      <ProductsList id={4} />
     </>
   )
 }
