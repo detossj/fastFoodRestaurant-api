1861135b71ab243a9776505427aa03884b26ba69
detossj <jorgerubilarssj@gmail.com>
2025-12-18 19:56:28 -0300

    added header in PromotionsList.jsx

--- resources/js/components/PromotionsList.jsx
+++ resources/js/components/PromotionsList.jsx
@@ -9,13 +9,32 @@ const PromotionsList = () => {
     if (loading) return <LoadingBar />;
 
     return (
+      <>
+        <div className="container px-5 pt-4">
+          <div className="row">
+            <div className="col-12">
+              <h1 className="fw-bold mb-1">
+                Promociones
+              </h1>
+
+              <p className="text-muted mb-0 col-md-8">
+                Combos y ofertas especiales en pizzas, hamburguesas, acompañamientos, bebidas y más.
+              </p>
+            </div>
+          </div>
+        </div>
+
         <div className="container p-5" style={{ minHeight: "100vh" }}>
           <div className="d-flex flex-column gap-4">
             {promotions.map(promotion => (
-              <PromotionCard key={promotion.id} promotion={promotion} />
+                <PromotionCard key={promotion.id} promotion={promotion} />
             ))}
           </div>
         </div>
+      
+      </>
+      
+        
     );
 }
 
