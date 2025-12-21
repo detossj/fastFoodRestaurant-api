df2d2dd107d46fdea161a1f771a33382e52958a1
detossj <jorgerubilarssj@gmail.com>
2025-12-21 13:47:38 -0300

    created AuthController and added functions

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register (Request $request) {
+
+        $response = ['success' => false];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+            'password_confirmation' => 'required|string|min:6',
+            'phone' => 'required|string|max:255',
+            'address' => 'required|string|max:255'
+        ]);
+
+        if($validator->fails()) {
+            $response = ['error' => $validator->errors()];
+            return response()->json($response, 200);
+        }
+
+        $validated = $validator->validated();
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+            'phone' => $validated['phone'],
+            'address' => $validated['address']
+        ]);
+
+        $response['token'] = $user->createToken('mobile')->plainTextToken;
+        $response['user'] = $user;
+        $response['success'] = true;
+
+        return response()->json($response, 201);
+    }
+
+    public function login (Request $request) {
+        
+        $response = ["success" => false];
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        
+        if ($validator->fails()) {
+            $response = ["error" => $validator->errors()];
+            return response()->json($response, 200);
+        }
+        
+
+        $user = User::where('email', $request->email)->first();
+
+        if(! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['El correo electrónico o la contraseña son incorrectos.'],
+            ]);
+        }
+
+        $response['token'] = $user->createToken('mobile')->plainTextToken;
+        $response['user'] = $user;
+        $response['message'] = "Login success";
+        $response['success'] = true;
+
+        return response()->json($response, 200);
+        
+    }
+
+    public function logout() {
+
+        $response = ['success' => false];
+
+        auth()->user()->tokens()->delete();
+
+        $response = [
+            'success' => true,
+            'message' => 'Sesión cerrada correctamente'
+        ];
+
+        return response()->json($response, 200);
+
+    }
+}
