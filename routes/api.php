fdb6a6261adb303e7dc314642ddfb4c0c169b220
detossj <jorgerubilarssj@gmail.com>
2025-12-21 13:47:55 -0300

    added auth Routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PromotionController;
@@ -12,6 +13,17 @@ use App\Http\Controllers\PromotionController;
 Route::get('/products', [ProductController::class,'allProducts']);
 Route::get('/categories', [CategoryController::class,'allCategories']);
 Route::get('/promotions', [PromotionController::class,'allPromotions']);
+
+// Auth
+Route::post('/auth/register',[AuthController::class, 'register']);
+Route::post('/auth/login',[AuthController::class, 'login']);
+
+// Private
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+    Route::post('/auth/logout',[AuthController::class, 'logout']);
+    
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
