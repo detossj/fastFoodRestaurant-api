b47a7c748ac96059acffb12e182aa203f99cf753
detossj <jorgerubilarssj@gmail.com>
2025-12-23 12:04:13 -0300

    added Login and Register in Config.jsx

--- resources/js/Config.jsx
+++ resources/js/Config.jsx
@@ -6,6 +6,9 @@ const API_URL = `${API_BASE_URL}/api`;
 
 export default {
     API_BASE_URL,
+     // AUTH
+    Login: (data) => axios.post(`${API_URL}/auth/login`, data),
+    Register: (data) => axios.post(`${API_URL}/auth/register`, data),
     getAllProducts: () => axios.get(`${API_URL}/products`),
     getAllCategories: () => axios.get(`${API_URL}/categories`),
     getAllPromotions: () => axios.get(`${API_URL}/promotions`),
