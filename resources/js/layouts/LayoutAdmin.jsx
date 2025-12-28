0d7c3624b434708e6bd6f537116e0d497f3c9651
detossj <jorgerubilarssj@gmail.com>
2025-12-28 12:27:25 -0300

    changed LayoutAdmin.jsx

--- resources/js/layouts/LayoutAdmin.jsx
+++ resources/js/layouts/LayoutAdmin.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import TopBar from '../components/TopBar'
+import Footer from '../components/Footer'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const LayoutAdmin = () => {
+
   return (
-    <div>LayoutAdmin</div>
+    <>
+    <TopBar/>
+    <Navbar/>
+      <Outlet/>
+    <Footer/>
+  </>
   )
 }
 
