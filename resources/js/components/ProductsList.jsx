02f1aa479453adf9fd30f083fc9626f78c61f98e
detossj <jorgerubilarssj@gmail.com>
2025-12-29 17:05:25 -0300

    added ProductModal to ProductsList.jsx

--- resources/js/components/ProductsList.jsx
+++ resources/js/components/ProductsList.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useProducts } from "../context/ProductsContext";
 import ProductCard from "./ProductCard";
 import LoadingBar from "./LoadingBar";
+import ProductModal from './ProductModal';
 
 const ProductsList = ({ id }) => {
   const { products, loading } = useProducts();
+  const [modal, setModal] = useState(false)
+  const [datamodal, setDataModal] = useState([])
 
   if (loading) return <LoadingBar />;
 
   const filtered = products.filter(p => p.category_id === id);
 
+  const showModal = (e, product) => {
+    e.preventDefault();
+    setDataModal(product);
+    setModal(true);
+  };
+
   return (
     <div className="container p-5" style={{ minHeight: "100vh" }}>
       <div className="d-flex flex-wrap gap-4 justify-content-center">
         {filtered.map(product => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard key={product.id} product={product} onClick={(e)=>showModal(e,product)}/>
+          
         ))}
+        {
+          modal && <ProductModal product={datamodal} close={setModal}/>
+        }
       </div>
     </div>
   );
