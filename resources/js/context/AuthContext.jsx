55fd54044f3e4bf061ed110c52dc48adff98134e
detossj <jorgerubilarssj@gmail.com>
2025-12-31 13:40:10 -0300

    changed sessionStorage for localStorage in AuthContext.jsx

--- resources/js/context/AuthContext.jsx
+++ resources/js/context/AuthContext.jsx
@@ -1,38 +1,41 @@
 import React from "react"
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
+
 
 const AuthContext = createContext(null)
 
 export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null)
+  const [user, setUser] = useState(null)
+  const [rol, setRol] = useState(null)
 
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token")
+    const storedUser = localStorage.getItem("user")
+    const storedRol = localStorage.getItem("rol")
 
-  const [token, setToken] = useState(
-    JSON.parse(sessionStorage.getItem("token"))
-  )
-  const [user, setUser] = useState(
-    JSON.parse(sessionStorage.getItem("user"))
-  )
-  const [rol, setRol] = useState(
-    JSON.parse(sessionStorage.getItem("rol"))
-  )
+    if (storedToken && storedUser) {
+      setToken(JSON.parse(storedToken))
+      setUser(JSON.parse(storedUser))
+      setRol(JSON.parse(storedRol))
+    }
+  }, [])
 
   const login = (user, token, rol) => {
-    sessionStorage.setItem("token", JSON.stringify(token))
-    sessionStorage.setItem("user", JSON.stringify(user))
-    sessionStorage.setItem("rol", JSON.stringify(rol))
+    localStorage.setItem("token", JSON.stringify(token))
+    localStorage.setItem("user", JSON.stringify(user))
+    localStorage.setItem("rol", JSON.stringify(rol))
 
     setToken(token)
     setUser(user)
     setRol(rol)
-
   }
 
   const logout = () => {
-    sessionStorage.clear()
+    localStorage.clear()
     setToken(null)
     setUser(null)
     setRol(null)
-    navigate("/")
   }
 
   return (
