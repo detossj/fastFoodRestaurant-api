a53f90b482508336ea16b1b3a841b7ae16306337
detossj <jorgerubilarssj@gmail.com>
2026-01-04 13:59:07 -0300

    changed design in PromotionCard.jsx

--- resources/js/components/PromotionCard.jsx
+++ resources/js/components/PromotionCard.jsx
@@ -3,38 +3,35 @@ import './PromotionCard.css';
 import Config from '../Config'
 
 const PromotionCard = ({ promotion }) => {
-    return (
-      <div className="promotion-card shadow rounded px-4 py-3 w-100">
-        <div className="promotion-img-wrapper mb-3">
-          <img 
-            src={`${Config.API_BASE_URL}/storage/promotions${promotion.image_url}`}
-            alt={promotion.name}
-            className="promotion-img"
-            loading='lazy'
-            decoding="async"
-          />
-        </div>
-  
-        <div className="promotion-info mt-2">
-            <h5 className="fw-bold text-center">{promotion.name}</h5>
-  
-            <p className="text-muted text-center small">
-              {promotion.description ?? 'Sin descripción disponible.'}
-            </p>
-        </div>
-  
-        <div className="promotion-bottom">
-            <p className="promotion-price text-center fw-semibold">
-              ${promotion.price}
-            </p>
-  
-            <button className="btn btn-primary px-4 promotion-button" >
-              Agregar
-            </button>
-        </div>
-  
+  return (
+    <div className="promotion-card shadow rounded p-3 h-100">
+
+      <div className="promotion-img-wrapper mb-3">
+        <img 
+          src={`${Config.API_BASE_URL}/storage/promotions${promotion.image_url}`}
+          alt={promotion.name}
+          className="promotion-img"
+          loading='lazy'
+          decoding="async"
+        />
       </div>
-    )
+
+      <div className="promotion-info mt-3">
+        <h5 className="fw-bold text-center">{promotion.name}</h5>
+
+        <p className="text-muted text-center small mt-3">
+          {promotion.description ?? 'Sin descripción disponible.'}
+        </p>
+      </div>
+
+      <div className="product-bottom">
+        <button className="add-cart-btn-card">
+          AGREGAR <span className="price">{promotion.price}</span>
+        </button>
+      </div>
+
+    </div>
+  )
 }
 
 export default PromotionCard
