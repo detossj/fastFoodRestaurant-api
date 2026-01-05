1129c34d5b0d85acb15747be1032e9ef0027d8be
detossj <jorgerubilarssj@gmail.com>
2026-01-05 16:13:42 -0300

    added useCart in Cart.jsx

--- resources/js/components/Cart.jsx
+++ resources/js/components/Cart.jsx
@@ -1,33 +1,11 @@
 import React, { useState } from 'react'
 import ItemCart from './ItemCart'
 import './Cart.css'
+import { useCart } from '../context/CartContext'
 
 const Cart = ({ onClose }) => {
 
-  const [products, setProducts] = useState([
-    { id: 1, name: 'Pizza Napolitana', price: 8900, quantity: 1, image_url: '/pizza-napolitana.webp' },
-    { id: 2, name: 'Hamburguesa Doble', price: 6200, quantity: 2, image_url: '/hamburguesa-doble.webp' },
-    { id: 3, name: 'Coca-Cola Zero 350ml', price: 1500, quantity: 1, image_url: '/coca-cola-zero-350ml.webp' },
-  ])
-
-  const increase = (id) => {
-    setProducts(products.map(p =>
-      p.id === id ? { ...p, quantity: p.quantity + 1 } : p
-    ))
-  }
-
-  const decrease = (id) => {
-    setProducts(products.map(p =>
-      p.id === id && p.quantity > 1
-        ? { ...p, quantity: p.quantity - 1 }
-        : p
-    ))
-  }
-
-  const total = products.reduce(
-    (acc, p) => acc + p.price * p.quantity,
-    0
-  )
+  const { cart, increaseQty, decreaseQty, total} = useCart()
 
   return (
     <div className="cart-overlay" onClick={onClose}>
@@ -44,12 +22,14 @@ const Cart = ({ onClose }) => {
         <h2>Tu pedido</h2>
 
         <div className="cart-items">
-          {products.map(product => (
+          {cart.length === 0 && <p>Carrito vacío</p>}
+
+          {cart.map(product => (
             <ItemCart
               key={product.id}
               product={product}
-              onIncrease={increase}
-              onDecrease={decrease}
+              onIncrease={increaseQty}
+              onDecrease={decreaseQty}
             />
           ))}
         </div>
