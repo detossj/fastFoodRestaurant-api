3369a41e549d23b879dc140f7473eb20782b9475
detossj <jorgerubilarssj@gmail.com>
2026-01-05 16:11:31 -0300

    created CartContext.jsx

---
+++ resources/js/context/CartContext.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+import { createContext, useContext, useState, useEffect } from "react"
+
+
+const CartContext = createContext()
+
+export const CartProvider = ({ children }) => {
+
+    const [cart, setCart] = useState(() => {
+        const savedCart = localStorage.getItem('cart')
+        return savedCart ? JSON.parse(savedCart) : []
+    })
+      
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+    const clearCart = () => {
+        setCart([])
+        localStorage.removeItem('cart')
+    }
+      
+      
+
+    const addToCart = (product) => {
+        setCart(prev => {
+        const exists = prev.find(p => p.id === product.id)
+
+        if (exists) {
+            return prev.map(p =>
+            p.id === product.id
+                ? { ...p, quantity: p.quantity + 1 }
+                : p
+            )
+        }
+
+        return [...prev, { ...product, quantity: 1 }]
+        })
+    }
+
+    const increaseQty = (id) => {
+        setCart(cart.map(p =>
+        p.id === id ? { ...p, quantity: p.quantity + 1 } : p
+        ))
+    }
+
+    const decreaseQty = (id) => {
+        setCart(cart.map(p =>
+        p.id === id && p.quantity > 1
+            ? { ...p, quantity: p.quantity - 1 }
+            : p
+        ))
+    }
+
+    const total = cart.reduce(
+        (acc, p) => acc + p.price * p.quantity,
+        0
+    )
+
+    const totalItems = cart.reduce(
+        (acc, p) => acc + p.quantity,
+        0
+    )
+
+    return (
+        <CartContext.Provider value={{
+        cart,
+        addToCart,
+        increaseQty,
+        decreaseQty,
+        clearCart,
+        total,
+        totalItems
+        }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export const useCart = () => useContext(CartContext)
