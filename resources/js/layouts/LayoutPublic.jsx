0a252bca1e6e4192e3274cc8a76fff365ea73c4d
detossj <jorgerubilarssj@gmail.com>
2026-01-07 15:23:33 -0300

    added LoadingBar in LayoutPublic.jsx

--- resources/js/layouts/LayoutPublic.jsx
+++ resources/js/layouts/LayoutPublic.jsx
@@ -3,14 +3,25 @@ import TopBar from '../components/TopBar'
 import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import { useProducts } from '../context/ProductsContext'
+import LoadingBar from '../components/LoadingBar'
 
 const LayoutPublic = () => {
+  const { loading, error } = useProducts()
+
   return (
     <>
-      <TopBar/>
-      <Navbar/>
-        <Outlet/>
-      <Footer/>
+      <TopBar />
+      <Navbar />
+      {loading ? (
+        <LoadingBar />
+      ) : error ? (
+        <p className="text-center mt-5">{error}</p>
+      ) : (
+        <Outlet />
+      )}
+
+      <Footer />
     </>
   )
 }
