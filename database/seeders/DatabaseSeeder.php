251f7eb76854f1d7339d3b66e975a0274c8cee99
detossj <jorgerubilarssj@gmail.com>
2026-01-13 14:00:36 -0300

    added StoreSeeder in DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\RolesSeeder;
+use Database\Seeders\StoreSeeder;
 use Database\Seeders\ProductSeeder;
 use Database\Seeders\CategorySeeder;
 use Database\Seeders\AdminUserSeeder;
@@ -38,6 +39,10 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             AdminUserSeeder::class
+        ]);
+
+        $this->call([
+            StoreSeeder::class
         ]);
 
     }
