32ac61024556b4f39647b7533b5fd714927b2bed
detossj <jorgerubilarssj@gmail.com>
2026-01-13 13:53:27 -0300

    created Store Model

---
+++ app/Models/Store.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    protected $fillable = [
+        'direction',
+        'sub_direction',
+        'phone',
+        'image_url',
+        'schedule'
+    ];
+
+}
