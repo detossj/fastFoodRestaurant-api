99a0efe36fd4278bd6941f6d4dd767919aa4bb1a
detossj <jorgerubilarssj@gmail.com>
2026-01-14 15:38:50 -0300

    created StoreList.jsx

---
+++ resources/js/components/StoreList.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import Config from '../Config'
+
+const StoreList = () => {
+
+    const [stores, setStores] = useState([])
+
+    const loadStores = async () => {
+        try {
+            const response = await Config.getAllStores()
+            setStores(response.data)
+            console.log(response.data)
+        } catch (error) {
+            console.error("Error:",error),
+            setStores([])
+        }
+    }
+
+    useEffect(() => {
+        loadStores();
+    },[])
+
+    return (
+        <div className='container p-5' style={{ minHeight: '100vh'}}>
+            <div className="d-flex flex-wrap gap-4 justify-content-center">
+
+            </div>
+        </div>
+    )
+}
+
+export default StoreList
