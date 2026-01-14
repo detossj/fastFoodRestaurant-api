ff2fb8c2d26f0284595996f8c5cc5f4200c9c7ca
detossj <jorgerubilarssj@gmail.com>
2026-01-14 15:38:42 -0300

    created StoreSectionHeader.jsx

---
+++ resources/js/components/StoreSectionHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const StoreSectionHeader = () => {
+  return (
+    <div className="container px-5 pt-4">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="fw-bold mb-1">
+            Nuestros Locales
+          </h1>
+
+          <p className="text-muted mb-0 col-md-8">
+            Estamos más cerca de ti para que disfrutes tu comida favorita sin esperar.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StoreSectionHeader
