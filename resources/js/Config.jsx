b919bd9268c225c681bc59f57aefd1bf4b47bb32
detossj <jorgerubilarssj@gmail.com>
2026-01-14 15:39:33 -0300

    added getAllStores in Config.jsx

--- resources/js/Config.jsx
+++ resources/js/Config.jsx
@@ -12,4 +12,5 @@ export default {
     getAllProducts: () => axios.get(`${API_URL}/products`),
     getAllCategories: () => axios.get(`${API_URL}/categories`),
     getAllPromotions: () => axios.get(`${API_URL}/promotions`),
+    getAllStores: () => axios.get(`${API_URL}/stores`),
 };
