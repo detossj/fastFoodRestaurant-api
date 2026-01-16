bf2a2b4d9d385c71a24ba42f698953ec4700d79c
detossj <jorgerubilarssj@gmail.com>
2026-01-16 11:40:41 -0300

    created ProfileSectionHeader.jsx

---
+++ resources/js/pages/ProfileSectionHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProfileSectionHeader = () => {
+  return (
+    <div className="container px-5 pt-4">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="fw-bold mb-1">
+            Perfil
+          </h1>
+
+          <p className="text-muted mb-0 col-md-8">
+            Aqui puedes cambiar los datos de tu cuenta.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileSectionHeader
