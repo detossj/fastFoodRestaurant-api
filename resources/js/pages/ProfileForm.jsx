7bbfcc09b6fd89c2d9d5b2a125355f0477abf7b8
detossj <jorgerubilarssj@gmail.com>
2026-01-18 21:45:49 -0300

    added states and functions in ProfileForm.jsx

--- resources/js/pages/ProfileForm.jsx
+++ resources/js/pages/ProfileForm.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuth } from '../context/AuthContext'
+import Config from '../Config';
 
 const ProfileForm = () => {
 
   const { user } = useAuth()
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+    phone: "",
+    address: "",
+  });
+
+  useEffect(() => {
+    if(user) {
+      setFormData({
+        name: user.name,
+        email: user.email,
+        password: "",
+        password_confirmation: "",
+        phone: user.phone,
+        address: user.address,
+      })
+    }
+  },[user])
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await Config.updateProfile(formData);
+      alert(response.data.message);
+    } catch (error) {
+      console.error(error);
+      alert("Error al actualizar el perfil");
+    }
+  };
 
   return (
     <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: "50vh" }}>
@@ -12,40 +54,40 @@ const ProfileForm = () => {
         <div className="col-12 mb-5">
             <div className="card shadow-sm border p-4">
 
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="row mb-3">
                   <div className="col-md-5">
                     <label className='form-label'>Nombre</label>
-                    <input value={user.name} className='form-control' type='text'/>
+                    <input name='name' value={formData.name} onChange={handleChange} className='form-control' type='text'/>
                   </div>
 
                   <div className="col-md-7">
                     <label className='form-label'>Correo</label>
-                    <input value={user.email} className='form-control' type='email'/>
+                    <input name='email' value={formData.email} onChange={handleChange} className='form-control' type='email'/>
                   </div>
                 </div>
 
                 <div className="row mb-3">
                   <div className="col-md-7">
                     <label className='form-label'>Dirección</label>
-                    <input value={user.address} className='form-control' type='text'/>
+                    <input name='address' value={formData.address} onChange={handleChange} className='form-control' type='text'/>
                   </div>
 
                   <div className="col-md-5">
                     <label className='form-label'>Teléfono</label>
-                    <input value={user.phone} className='form-control' type='email'/>
+                    <input name='phone' value={formData.phone} onChange={handleChange} className='form-control' type='text'/>
                   </div>
                 </div>
 
                 <div className="row mb-3">
                   <div className="col-md-6">
                     <label className='form-label'>Contraseña</label>
-                    <input className='form-control' type='password'/>
+                    <input name="password" onChange={handleChange} className='form-control' type='password'/>
                   </div>
 
                   <div className="col-md-6">
                     <label className='form-label'>Confirmar contraseña</label>
-                    <input className='form-control' type='password'/>
+                    <input name="password_confirmation" onChange={handleChange} className='form-control' type='password'/>
                   </div>
                 </div>
 
