cc6cd48b4d9ee23e74614562ab892f7242628823
detossj <jorgerubilarssj@gmail.com>
2026-01-18 21:46:24 -0300

    added interceptor request and updateProfile function in Config.jsx

--- resources/js/Config.jsx
+++ resources/js/Config.jsx
@@ -1,16 +1,36 @@
 import axios from "axios";
 
 const API_BASE_URL = "http://localhost:8000";
-
 const API_URL = `${API_BASE_URL}/api`;
+
+// es mejor hacer esto que axios.interceptors.request.use... porque haciendolo con axios afecta todas las request del proyecto
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token"); 
+  
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  
+    return config;
+});
 
 export default {
     API_BASE_URL,
-     // AUTH
-    Login: (data) => axios.post(`${API_URL}/auth/login`, data),
-    Register: (data) => axios.post(`${API_URL}/auth/register`, data),
-    getAllProducts: () => axios.get(`${API_URL}/products`),
-    getAllCategories: () => axios.get(`${API_URL}/categories`),
-    getAllPromotions: () => axios.get(`${API_URL}/promotions`),
-    getAllStores: () => axios.get(`${API_URL}/stores`),
+  
+    // AUTH
+    Login: (data) => api.post("/auth/login", data),
+    Register: (data) => api.post("/auth/register", data),
+    updateProfile: (data) => api.patch("/auth/profile", data),
+  
+    // PUBLIC / PRIVATE
+    getAllProducts: () => api.get("/products"),
+    getAllCategories: () => api.get("/categories"),
+    getAllPromotions: () => api.get("/promotions"),
+    getAllStores: () => api.get("/stores"),
 };
+  
+  
