a65167ffb5cdc74b887028ed7744a32cce3e1aff
detossj <jorgerubilarssj@gmail.com>
2026-01-20 09:10:41 -0300

    created OrderSectionHeader.jsx

---
+++ resources/js/components/OrderSectionHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const OrderSectionHeader = () => {
+  return (
+    <div className="container px-5 pt-4">
+    <div className="row">
+      <div className="col-12">
+        <h1 className="fw-bold mb-1">
+          Pedidos
+        </h1>
+
+        <p className="text-muted mb-0 col-md-8">
+          Aqui encontraras tus pedidos realizados anteriormente.
+        </p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default OrderSectionHeader
