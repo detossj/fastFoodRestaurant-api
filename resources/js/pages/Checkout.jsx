cd3bac65227fe65979df0d89a3b5e326a369c4fc
detossj <jorgerubilarssj@gmail.com>
2026-01-21 21:33:56 -0300

    changed grid design in Checkout.jsx

--- resources/js/pages/Checkout.jsx
+++ resources/js/pages/Checkout.jsx
@@ -3,7 +3,48 @@ import CheckoutTopBar from '../components/CheckoutTopBar'
 
 const Checkout = () => {
   return (
-    <CheckoutTopBar/>
+    <>
+      <CheckoutTopBar />
+
+      <div className="container py-5">
+        <div className="row g-4">
+
+          <div className="col-8">
+            
+            <div className="card shadow-sm mb-4">
+              <div className="card-body">
+                <h5 className="fw-bold mb-3">Retiro o Delivery</h5>
+
+              </div>
+            </div>
+
+            <div className="card shadow-sm mb-4">
+              <div className="card-body">
+                <h5 className="fw-bold mb-3">Datos del cliente</h5>
+
+              </div>
+            </div>
+
+            <div className="card shadow-sm">
+              <div className="card-body">
+                <h5 className="fw-bold mb-3">Método de pago</h5>
+              </div>
+            </div>
+
+          </div>
+
+
+          <div className="col-4">
+            <div className="card shadow-sm position-sticky" style={{ top: "90px" }}>
+              <div className="card-body">
+                <h5 className="fw-bold mb-3">Resumen</h5>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
