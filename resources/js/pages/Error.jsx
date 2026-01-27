c8dda00c9594949201df555a9fee72a1596c22a7
Jorge Rubilar <jorgerubilarssj@gmail.com>
2026-01-27 09:18:33 -0300

    added CheckoutTopBar and ErrorCard in Error.jsx

---
+++ resources/js/pages/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CheckoutTopBar from '../components/CheckoutTopBar'
+import ErrorCard from '../components/ErrorCard'
+
+const Error = () => {
+  return (
+    <>
+      <CheckoutTopBar />
+      <ErrorCard />
+    </>
+  )
+}
+
+export default Error
