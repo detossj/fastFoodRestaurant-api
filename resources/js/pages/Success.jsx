1cd7e05e428cf1f3bb3132f78f8cc55d12cff708
Jorge Rubilar <jorgerubilarssj@gmail.com>
2026-01-27 09:37:22 -0300

    added CheckoutTopBar and SuccessCard in Success.jsx

---
+++ resources/js/pages/Success.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CheckoutTopBar from '../components/CheckoutTopBar'
+import SuccessCard from '../components/SuccessCard'
+
+
+const Success = () => {
+  return (
+    <>
+      <CheckoutTopBar />
+      <SuccessCard />
+    </>
+  )
+}
+
+export default Success
