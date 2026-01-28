1829171c2c40e6a6ca416c14f893ce562fe0240c
Jorge Rubilar <jorgerubilarssj@gmail.com>
2026-01-28 10:10:16 -0300

    changed styles and className in CheckoutTopBar

--- resources/js/components/CheckoutTopBar.jsx
+++ resources/js/components/CheckoutTopBar.jsx
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom"
 
 const CheckoutTopBar = () => {
   return (
-    <div className="py-2" style={{ borderBottom: "1px solid #e5e5e5" }}>
+    <div className="py-3" style={{ borderBottom: "1px solid #e5e5e5" }}>
       <div className="container d-flex justify-content-center align-items-center">
 
         <NavLink to="/">
-          <img src={logo} alt="Logo" style={{ height: "70px" }} />
+          <img src={logo} alt="Logo" style={{ height: "90px" }} />
         </NavLink>
         
       </div>
