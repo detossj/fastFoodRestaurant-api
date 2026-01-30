dbd8df3b89c09804a72317da1837807b11fa77f6
Jorge Rubilar <jorgerubilarssj@gmail.com>
2026-01-30 10:06:47 -0300

    added test function in Pedidos.jsx

--- resources/js/pages/Pedidos.jsx
+++ resources/js/pages/Pedidos.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import OrderSectionHeader from '../components/OrderSectionHeader'
+import Config from '../Config';
 
 const Pedidos = () => {
+  const Pedido = async () => {
+
+    try {
+      const response = await Config.getOrders();
+      if (response.status === 201 || response.status === 200) {
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.error("Error al obtener pedidos:", error.response?.data);
+
+    }
+
+  }
+
   return (
-    <OrderSectionHeader/>
+    <>
+      <OrderSectionHeader/>
+
+      <button onClick={() => Pedido()}>Crear Pedido</button>
+    </>
   )
 }
 
