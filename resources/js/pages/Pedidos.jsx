182ede6057dd96eea7f77e9228191f797c3e6f1d
detossj <jorgerubilarssj@gmail.com>
2026-01-31 18:36:54 -0300

    added OrderList to Pedidos.jsx

--- resources/js/pages/Pedidos.jsx
+++ resources/js/pages/Pedidos.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
 import OrderSectionHeader from '../components/OrderSectionHeader'
-import Config from '../Config';
+import OrderList from '../components/OrderList';
 
 const Pedidos = () => {
-  const Pedido = async () => {
-
-    try {
-      const response = await Config.getOrders();
-      if (response.status === 201 || response.status === 200) {
-        console.log(response.data);
-      }
-    } catch (error) {
-      console.error("Error al obtener pedidos:", error.response?.data);
-
-    }
-
-  }
-
   return (
     <>
       <OrderSectionHeader/>
-
-      <button onClick={() => Pedido()}>Crear Pedido</button>
+      <OrderList/>
     </>
   )
 }
