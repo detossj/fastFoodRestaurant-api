c5ce10cb463290aa8b79b6d271372a232033ae1f
Jorge Rubilar <jorgerubilarssj@gmail.com>
2026-02-05 11:39:10 -0300

    changed design in OrdenProductCard.jsx

--- resources/js/components/OrderProductCard.jsx
+++ resources/js/components/OrderProductCard.jsx
@@ -3,33 +3,18 @@ import Config from '../Config';
 import './OrderProductCard.css';
 
 const OrderProductCard = ({ item, type }) => {
-
   const data = type === 'product' ? item.product : item.promotion;
 
   return (
-    <div className="order-product-card">
-
-      <div className="order-product-img-wrapper">
-        <img
-          src={`${Config.API_BASE_URL}/storage${data.image_url}`}
-          alt={data.name}
-          className="order-product-img"
-          loading="lazy"
-        />
+    <div className="mini-product-card">
+      <img 
+        src={`${Config.API_BASE_URL}/storage${data.image_url}`} 
+        alt={data.name} 
+      />
+      <div className="mini-info">
+        <p className="mini-name">{data.name}</p>
+        <p className="mini-qty">x{item.quantity} — <strong>${Math.trunc(item.price)}</strong></p>
       </div>
-
-      <div className="order-product-info">
-        <h6 className="fw-bold text-center">{data.name}</h6>
-
-        <p className="text-muted small text-center">
-          Cantidad: {item.quantity}
-        </p>
-
-        <p className="order-product-price text-center">
-          ${Math.trunc(item.price)}
-        </p>
-      </div>
-
     </div>
   );
 };
