dce71a36cd411e12283180d2c040afb8b84273cf
Jorge Rubilar <jorgerubilarssj@gmail.com>
2026-02-06 09:34:05 -0300

    added loading state in PublicRoutes.jsx

--- resources/js/pageAuth/PublicRoutes.jsx
+++ resources/js/pageAuth/PublicRoutes.jsx
@@ -3,10 +3,12 @@ import { Navigate, Outlet, useLocation } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 
 const PublicRoutes = () => {
-  const { token, rol } = useAuth()
+  const { token, rol, loading } = useAuth()
   const location = useLocation()
 
   const authPages = ["/login", "/register"]
+
+  if (loading) return null;
 
   if (token && authPages.includes(location.pathname)) {
     return <Navigate to={rol === "admin" ? "/admin" : "/"} replace />
