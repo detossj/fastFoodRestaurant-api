ade685ed1704a3c86734d7ef21b96f903b3723ef
deto <jorgerubilarssj@gmail.com>
2026-02-07 12:50:27 -0300

    created Gestionar.jsx and added ManageList

---
+++ resources/js/pages/Gestionar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ManageList from '../components/ManageList'
+
+const Gestionar = () => {
+  return (
+    <>
+      <ManageList/>
+    </>
+  )
+}
+
+export default Gestionar
