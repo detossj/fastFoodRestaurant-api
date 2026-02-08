bac96164a41aee8a372b0110661d38cf197db35d
detossj <jorgerubilarssj@gmail.com>
2026-02-08 15:54:07 -0300

    created OrderContext.jsx

---
+++ resources/js/context/OrderContext.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { createContext } from "react";
+import Config from '../Config';
+
+const OrdersContext = createContext(null)
+
+
+export const OrderProvider = ({ children }) => {
+
+    const [orders, setOrders] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+
+    const loadData = async () => {
+        setLoading(true);
+        setError(null);
+    
+        try {
+          const ordersRes = await Config.getOrders()
+    
+          setOrders(ordersRes?.data ?? []);
+    
+        } catch (err) {
+            console.error("Error cargando datos:", err);
+            setOrders([])
+            setError("Error al cargar los pedidos");
+        } finally {
+            setLoading(false);
+        }
+    };
+    
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    return (
+        <OrdersContext.Provider value={{ orders, loading, error }}>
+            {children}
+        </OrdersContext.Provider>
+    )
+}
+
+export const useOrders = () => useContext(OrdersContext);
