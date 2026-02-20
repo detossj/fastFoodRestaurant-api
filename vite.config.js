2b62e07df061fb82cced1c40757225d3acb38c69
detossj <jorgerubilarssj@gmail.com>
2026-02-20 18:19:34 -0300

    added server in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',   
+        port: 5173,
+        hmr: {
+            host: '192.168.1.89', // IPV4 PC
+        },
+    },
     plugins: [
         laravel({
             input: [
