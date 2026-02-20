c7261f042b25430f46294e066ae9a606d010b527
Jorge Rubilar <jorgerubilarssj@gmail.com>
2026-02-20 09:46:37 -0300

    added conditional in OrderList.jsx

--- resources/js/components/OrderList.jsx
+++ resources/js/components/OrderList.jsx
@@ -4,19 +4,29 @@ import { useOrders } from '../context/OrderContext';
 
 const OrderList = () => {
 
-    const { orders } = useOrders()
+    const { orders, loading } = useOrders()
 
     return (
         <div className='container p-5' style={{ minHeight: '100vh'}}>
             <div className="d-flex flex-wrap gap-4 justify-content-center">
-                {
+                
+                {loading ? (
+                    <h4 className="text-muted text-center mt-5">Cargando pedidos...</h4>
+                ) : 
+
+                orders.length === 0 ? (
+                    <h4 className="text-muted text-center mt-5">No tienes pedidos actualmente.</h4>
+                ) : 
+                
+                (
                     orders.map( order => (
                         <OrderCard key={order.id} order={order}/>
                     ))
-                }
+                )}
+                
             </div>
         </div>
     )
 }
 
-export default OrderList
+export default OrderList;
