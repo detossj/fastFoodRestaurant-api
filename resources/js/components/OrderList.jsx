896d2a23394dd8c68b0d9b2ee36a76bc058f3717
Jorge Rubilar <jorgerubilarssj@gmail.com>
2026-02-20 10:00:21 -0300

    added loadData function in OrderList.jsx

--- resources/js/components/OrderList.jsx
+++ resources/js/components/OrderList.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import OrderCard from './OrderCard';
 import { useOrders } from '../context/OrderContext';
 
 const OrderList = () => {
 
-    const { orders, loading } = useOrders()
+    const { orders, loading, loadData } = useOrders()
+
+    useEffect(() => {
+        loadData();
+    }, []); 
 
     return (
         <div className='container p-5' style={{ minHeight: '100vh'}}>
@@ -13,11 +17,9 @@ const OrderList = () => {
                 {loading ? (
                     <h4 className="text-muted text-center mt-5">Cargando pedidos...</h4>
                 ) : 
-
                 orders.length === 0 ? (
                     <h4 className="text-muted text-center mt-5">No tienes pedidos actualmente.</h4>
                 ) : 
-                
                 (
                     orders.map( order => (
                         <OrderCard key={order.id} order={order}/>
