273d06da67613c06c1383ea11c871b4aee6fc4e4
detossj <jorgerubilarssj@gmail.com>
2026-02-24 22:21:31 -0300

    added env variable in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    server: {
-        host: '0.0.0.0',   
-        port: 5173,
-        hmr: {
-            host: '192.168.1.89', // IPV4 PC
+
+export default defineConfig(({ mode }) => {
+
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        server: {
+            host: '0.0.0.0',   
+            port: 5173,
+            hmr: {
+                host: env.VITE_IP_LOCAL, 
+            },
         },
-    },
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        //react(),
-    ],
+        plugins: [
+            laravel({
+                input: [
+                    'resources/sass/app.scss',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+            // react(),
+        ],
+    };
 });
