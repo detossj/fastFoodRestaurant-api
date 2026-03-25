9909be42b8fcb72d6f691a21d1caa8dffcd19aec
detossj <jorgerubilarssj@gmail.com>
2026-03-25 11:52:59 -0300

    Se agrego php artisan storage:link en docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "🚀 Optimizando configuración de Laravel..."
+echo "Optimizando configuración de Laravel..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-echo "📦 Ejecutando migraciones de la base de datos..."
-# El --force es obligatorio en producción para que no pida confirmación
+echo "Ejecutando migraciones de la base de datos..."
 php artisan migrate --force
 
-echo "🌱 Ejecutando seeders..."
+echo "Ejecutando seeders..."
 php artisan db:seed --force 
 
-echo "🌐 Iniciando servidor Apache..."
+
+echo "🔗 Creando enlace de almacenamiento para las imágenes..."
+php artisan storage:link --force
+
+echo "Iniciando servidor Apache..."
 exec apache2-foreground
